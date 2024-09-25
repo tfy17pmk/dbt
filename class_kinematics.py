77394fdaf91b86b39b46fc76b95828c46e471488
Polly <polly@cs.umu.se>
2024-09-25 13:16:31 +0200

    Review modifications.

--- class_kinematics.py
+++ class_kinematics.py
@@ -3,101 +3,117 @@ import numpy as np
 class Kinematics:
     # setup init
     def __init__(self):
-        # define lengths [origin to servo, first arm, second arm, radius of hexagon]
-        self.L = [0.04, 0.04, 0.065, 0.065]
-        # initial pose of table (theta, phi, h)
+        # Lengths for the robot arms and platform:
+        # lengths = [origin to servo, first arm, second arm, radius of hexagonal platform]
+        self.lengths = [0.04, 0.04, 0.065, 0.065]
+
+        # Initial position of table (theta, phi, height)
         self.initial_position = [0, 0, 0.0632]
-        self.h_max = 0.0732 # max height
-        self.h_min = 0.0532 # min height
-        self.phi_max = 20 # max angle
 
-    def InverseKinematics(self, n, h):
-        L = self.L
+        # High limits for the platform
+        self.height_max = 0.0732 
+        self.height_min = 0.0532 
 
-        # Set reference height for leg
-        A = (L[0] + L[1]) / h
-        B = (h**2 + L[2]**2 - (L[0] + L[1])**2 - L[3]**2) / (2*h)
+        # Maximum tilt angle
+        self.phi_max = 20
+
+    def inverse_kinematics(self, normal_vector, height):
+        base_to_servo_length = self.lengths[0]
+        arm_1_length = self.lengths[1]
+        arm_2_length = self.lengths[2]
+        platform_radius = self.lengths[3]
+        initial_orientation = self.initial_position[:2]
+        initial_height = self.initial_position[2]
+        max_tilt_angle = self.phi_max
+
+        # Set reference height for arms (A-E are intermediate variables for simplified calculations)
+        A = (base_to_servo_length + arm_1_length) / height
+        B = (height**2 + arm_2_length**2 - (base_to_servo_length + arm_1_length)**2 - platform_radius**2) / (2*height)
         C = A**2 + 1
-        D = 2 * (A * B - (L[0] + L[1]))
-        E = B**2 + (L[0] + L[1])**2 - L[2]**2
+        D = 2 * (A * B - (base_to_servo_length + arm_1_length))
+        E = B**2 + (base_to_servo_length + arm_1_length)**2 - arm_2_length**2
 
         Pmx = (-D + np.sqrt(D**2 - 4 * C * E)) / (2 * C)
-        Pmz = np.sqrt(L[2]**2 - Pmx**2 + 2 * (L[0] + L[1]) * Pmx - (L[0] + L[1])**2)
+        Pmz = np.sqrt(arm_2_length**2 - Pmx**2 + 2 * (base_to_servo_length + arm_1_length) * Pmx - (base_to_servo_length + arm_1_length)**2)
 
-        # First leg
-        # coordinates of mounting point for a1 arm on table
-        a1_x_mount = n[2] * L[3] / np.sqrt(n[0]**2 + n[2]**2)
-        a1_y_mount = 0
-        a1_z_mount = h + n[0] * L[3] / np.sqrt(n[0]**2 + n[2]**2)
-        A1_mount = [a1_x_mount, a1_y_mount, a1_z_mount]
+        """ --------------------------------------- FIRST ARM --------------------------------------- """
+        # Mounting point on the platform
+        arm_1_x_mount = normal_vector[2] * platform_radius / np.sqrt(normal_vector[0]**2 + normal_vector[2]**2)
+        arm_1_y_mount = 0
+        arm_1_z_mount = height + normal_vector[0] * platform_radius / np.sqrt(normal_vector[0]**2 + normal_vector[2]**2)
+        arm_1_mount_array = [arm_1_x_mount, arm_1_y_mount, arm_1_z_mount]
 
-        # Define knee coordinates
-        A1 = (L[0] - a1_x_mount) / a1_z_mount
-        B1 = -(a1_x_mount**2 + a1_z_mount**2 + L[1]**2 - L[0]**2 - L[2]**2)
+        # Define knee coordinates (A1-E1 are intermediate variables for simplified calculations)
+        A1 = (base_to_servo_length - arm_1_x_mount) / arm_1_z_mount
+        B1 = -(arm_1_x_mount**2 + arm_1_z_mount**2 + arm_1_length**2 - base_to_servo_length**2 - arm_2_length**2)
         C1 = A1**2 + 1
-        D1 = 2 * (A1 * B1 + L[0])
-        E1 = B1**2 - L[0]**2 - L[1]**2
+        D1 = 2 * (A1 * B1 + base_to_servo_length)
+        E1 = B1**2 - base_to_servo_length**2 - arm_1_length**2
 
+        arm_1_x_knee = (-D1 + np.sqrt(D1**2 - 4 * C1 * E1)) / (2 * C1)
+        arm_1_y_knee = 0
+        arm_1_z_knee = np.sqrt(arm_1_length**2 - (arm_1_x_knee - base_to_servo_length)**2)
 
-        a1_x_knee = (-D1 + np.sqrt(D1**2 - 4 * C1 * E1)) / (2 * C1)
-        a1_y_knee = 0
-        a1_z_knee = np.sqrt(L[1]**2 - (a1_x_knee - L[0])**2)
+        if (arm_1_z_mount < Pmz):
+            arm_1_z_knee = -arm_1_z_knee
+        arm_1_knee_array = [arm_1_x_knee, arm_1_y_knee, arm_1_z_knee]
 
-        if (a1_z_mount < Pmz):
-            a1_z_knee = -a1_z_knee
-        A1_knee = [a1_x_knee, a1_y_knee, a1_z_knee]
+        arm_1_costheta = (arm_1_knee_array[0] - base_to_servo_length) / arm_1_length
+        arm_1_sintheta = (arm_1_knee_array[2]) / arm_1_length
 
-        a1_costheta = (A1_knee[0] - L[0]) / L[1]
-        a1_sintheta = (A1_knee[2]) / L[1]
+        arm_1_theta = np.arctan2(arm_1_costheta, arm_1_sintheta)
 
-        a1_theta = np.arctan2(a1_costheta, a1_sintheta)
+        """ --------------------------------------- SECOND ARM --------------------------------------- """
+        # Mounting point on the platform
+        arm_2_x_mount = (-platform_radius * normal_vector[2]) / np.sqrt(normal_vector[0]**2 + 3 * normal_vector[1]**2 + 4 * normal_vector[2]**2 - 2 * np.sqrt(3) * normal_vector[0] * normal_vector[1])
+        arm_2_y_mount = (np.sqrt(3) * platform_radius * normal_vector[2]) / np.sqrt(normal_vector[0]**2 + 3 * normal_vector[1]**2 + 4 * normal_vector[2]**2 - 2 * np.sqrt(3) * normal_vector[0] * normal_vector[1])
+        arm_2_z_mount = height + (platform_radius * (-np.sqrt(3) * normal_vector[0] + normal_vector[1])) / np.sqrt(normal_vector[0]**2 + 3 * normal_vector[1]**2 + 4 * normal_vector[2]**2 - 2 * np.sqrt(3) * normal_vector[0] * normal_vector[1])
+        arm_2_mount_array = [arm_2_x_mount, arm_2_y_mount, arm_2_z_mount]
 
-        # Second leg
-        a2_x_mount = (-L[3] * n[2]) / np.sqrt(n[0]**2 + 3 * n[1]**2 + 4 * n[2]**2 - 2 * np.sqrt(3) * n[0] * n[1])
-        a2_y_mount = (np.sqrt(3) * L[3] * n[2]) / np.sqrt(n[0]**2 + 3 * n[1]**2 + 4 * n[2]**2 - 2 * np.sqrt(3) * n[0] * n[1])
-        a2_z_mount = h + (L[3] * (-np.sqrt(3) * n[0] + n[1])) / np.sqrt(n[0]**2 + 3 * n[1]**2 + 4 * n[2]**2 - 2 * np.sqrt(3) * n[0] * n[1])
-        A2_mount = [a2_x_mount, a2_y_mount, a2_z_mount]
-
-        # Define knee coordinates
-        A2 = (-A2_mount[0] + np.sqrt(3) * A2_mount[1] - 2 * L[0]) / A2_mount[2]
-        B2 = (A2_mount[0]**2 + A2_mount[1]**2 + A2_mount[2]**2 + L[1]**2 - L[0]**2 - L[2]**2) / (2 * A2_mount[2])
+        # Define knee coordinates (A2-E2 are intermediate variables for simplified calculations)
+        A2 = (-arm_2_mount_array[0] + np.sqrt(3) * arm_2_mount_array[1] - 2 * base_to_servo_length) / arm_2_mount_array[2]
+        B2 = (arm_2_mount_array[0]**2 + arm_2_mount_array[1]**2 + arm_2_mount_array[2]**2 + arm_1_length**2 - base_to_servo_length**2 - arm_2_length**2) / (2 * arm_2_mount_array[2])
         C2 = A2**2 + 4 
-        D2 = 2 * A2 * B2 + 4 * L[0]
-        E2 = B2**2 + L[0]**2 - L[1]**2
+        D2 = 2 * A2 * B2 + 4 * base_to_servo_length
+        E2 = B2**2 + base_to_servo_length**2 - arm_1_length**2
 
-        a2_x_knee = (-D2 - np.sqrt(D2**2 - 4 * C2 * E2)) / (2 * C2)
-        a2_y_knee = -np.sqrt(3) * a2_x_knee
-        a2_z_knee = np.sqrt(L[1]**2 - 4 * a2_x_knee**2 - 4 * L[0] * a2_x_knee - L[0]**2)
+        arm_2_x_knee = (-D2 - np.sqrt(D2**2 - 4 * C2 * E2)) / (2 * C2)
+        arm_2_y_knee = -np.sqrt(3) * arm_2_x_knee
+        arm_2_z_knee = np.sqrt(arm_1_length**2 - 4 * arm_2_x_knee**2 - 4 * base_to_servo_length * arm_2_x_knee - base_to_servo_length**2)
 
-        if (a2_z_mount < Pmz):
-            a2_z_knee = -a2_z_knee
-        A2_knee = [a2_x_knee, a2_y_knee, a2_z_knee]
+        if (arm_2_z_mount < Pmz):
+            arm_2_z_knee = -arm_2_z_knee
+        arm_2_knee_array = [arm_2_x_knee, arm_2_y_knee, arm_2_z_knee]
 
-        a2_theta = np.arctan2(A2_knee[2]**2, np.sqrt(A2_knee[0]**2 + A2_knee[1]**2) - L[0])
+        arm_2_theta = np.arctan2(arm_2_knee_array[2]**2, np.sqrt(arm_2_knee_array[0]**2 + arm_2_knee_array[1]**2) - base_to_servo_length)
 
+        """ --------------------------------------- THIRD ARM --------------------------------------- """
+        # Mounting point on the platform
+        arm_3_x_mount = (-platform_radius * normal_vector[2]) / np.sqrt(normal_vector[0]**2 + 3 * normal_vector[1]**2 + 4 * normal_vector[2]**2 - 2 * np.sqrt(3) * normal_vector[0] * normal_vector[1])
+        arm_3_y_mount = (-np.sqrt(3) * platform_radius * normal_vector[2]) / np.sqrt(normal_vector[0]**2 + 3 * normal_vector[1]**2 + 4 * normal_vector[2]**2 - 2 * np.sqrt(3) * normal_vector[0] * normal_vector[1])
+        arm_3_z_mount = height + (platform_radius * (np.sqrt(3) * normal_vector[0] + normal_vector[1])) / np.sqrt(normal_vector[0]**2 + 3 * normal_vector[1]**2 + 4 * normal_vector[2]**2 - 2 * np.sqrt(3) * normal_vector[0] * normal_vector[1])
+        arm_3_mount_array = [arm_3_x_mount, arm_3_y_mount, arm_3_z_mount]
 
-        # Third leg
-        a3_x_mount = (-L[3] * n[2]) / np.sqrt(n[0]**2 + 3 * n[1]**2 + 4 * n[2]**2 - 2 * np.sqrt(3) * n[0] * n[1])
-        a3_y_mount = (-np.sqrt(3) * L[3] * n[2]) / np.sqrt(n[0]**2 + 3 * n[1]**2 + 4 * n[2]**2 - 2 * np.sqrt(3) * n[0] * n[1])
-        a3_z_mount = h + (L[3] * (np.sqrt(3) * n[0] + n[1])) / np.sqrt(n[0]**2 + 3 * n[1]**2 + 4 * n[2]**2 - 2 * np.sqrt(3) * n[0] * n[1])
-        A3_mount = [a3_x_mount, a3_y_mount, a3_z_mount]
-
-        # Define knee coordinates
-        A3 = -(A3_mount[0] + np.sqrt(3) * A3_mount[1] + 2 * L[0]) / A3_mount[2]
-        B3 = (A3_mount[0]**2 + A3_mount[1]**2 + A3_mount[2]**2 + L[1]**2 - L[0]**2 - L[2]**2) / (2 * A3_mount[2])
+        # Define knee coordinates (A3-E3 are intermediate variables for simplified calculations)
+        A3 = -(arm_3_mount_array[0] + np.sqrt(3) * arm_3_mount_array[1] + 2 * base_to_servo_length) / arm_3_mount_array[2]
+        B3 = (arm_3_mount_array[0]**2 + arm_3_mount_array[1]**2 + arm_3_mount_array[2]**2 + arm_1_length**2 - base_to_servo_length**2 - arm_2_length**2) / (2 * arm_3_mount_array[2])
         C3 = A3**2 + 4 
-        D3 = 2 * A3 * B3 + 4 * L[0]
-        E3 = B3**2 + L[0]**2 - L[1]**2
+        D3 = 2 * A3 * B3 + 4 * base_to_servo_length
+        E3 = B3**2 + base_to_servo_length**2 - arm_1_length**2
 
-        a3_x_knee = (-D3 - np.sqrt(D3**2 - 4 * C3 * E3)) / (2 * C3)
-        a3_y_knee = np.sqrt(3) * a3_x_knee
-        a3_z_knee = np.sqrt(L[1]**2 - 4 * a3_x_knee**2 - 4 * L[0] * a3_x_knee - L[0]**2)
+        arm_3_x_knee = (-D3 - np.sqrt(D3**2 - 4 * C3 * E3)) / (2 * C3)
+        arm_3_y_knee = np.sqrt(3) * arm_3_x_knee
+        arm_3_z_knee = np.sqrt(arm_1_length**2 - 4 * arm_3_x_knee**2 - 4 * base_to_servo_length * arm_3_x_knee - base_to_servo_length**2)
 
-        if (a3_z_mount < Pmz):
-            a3_z_knee = -a3_z_knee
-        A3_knee = [a3_x_knee, a3_y_knee, a3_z_knee]
+        if (arm_3_z_mount < Pmz):
+            arm_3_z_knee = -arm_3_z_knee
+        arm_3_knee_array = [arm_3_x_knee, arm_3_y_knee, arm_3_z_knee]
 
-        a3_theta = np.arctan2(A3_knee[2]**2, np.sqrt(A3_knee[0]**2 + A3_knee[1]**2) - L[0])
+        arm_3_theta = np.arctan2(arm_3_knee_array[2]**2, np.sqrt(arm_3_knee_array[0]**2 + arm_3_knee_array[1]**2) - base_to_servo_length)
 
-        thetas = [a1_theta, a2_theta, a3_theta]
+        """ ---------------------------------------------------------------------------------------- """
+
+        # Return the three calculated theta values for the arms
+        thetas = [arm_1_theta, arm_2_theta, arm_3_theta]
+
         return thetas
