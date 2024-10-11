32aa69e12463a09cca5f9353d4aa2456f08f9614
malin00gidlund <malin00gidlund@gmail.com>
2024-10-11 12:08:23 +0200

    Add motor class with set_angle function

--- class_motors.py
+++ class_motors.py
@@ -1,43 +1,52 @@
 import pigpio
-
-# Connect to pigpiod daemon
-pi = pigpio.pi()
+import numpy as np
 
 class Motors:
-    def __init__(self):
-        pass
+    def __init__(self, MODE, DIR, STEP):
+        # Connect to pigpiod daemon
+        self.pi = pigpio.pi()
         
-    def setup_motor(self, resolution):
-        MODE = (14, 15, 18)   # Microstep Resolution GPIO Pins
+        self.MODE = MODE
+        self.DIR = DIR
+        self.STEP = STEP
+        
+        self.degree_per_step = 1.8/32
+        self.PWM_frequency = 8000
+        self.PWM = 128
 
-        RESOLUTION = {'Full': (0, 0, 0),
-                      'Half': (1, 0, 0),
-                      '1/4': (0, 1, 0),
-                      '1/8': (1, 1, 0),
-                      '1/16': (0, 0, 1),
-                      '1/32': (1, 0, 1)}
+        RESOLUTION = {'1/32': (1, 0, 1)}
+        for i in range(3):
+            self.pi.write(self.MODE[i], RESOLUTION['1/32'][i])
+            self.pi.set_mode(self.DIR[i], pigpio.OUTPUT)
+            self.pi.set_mode(self.STEP[i], pigpio.OUTPUT)
+            self.pi.set_PWM_dutycycle(self.STEP[i], 0)
+            self.pi.set_PWM_frequency(self.STEP[i], self.PWM_frequency)
+
+    def initial_position(self):
+        self.current_angles = np.zeros(3)
+
+    def set_angle(self, goal_angles):
+        relative_angles = goal_angles - self.current_angles
+        self.current_angles = goal_angles
+        required_steps = abs(relative_angles) / self.degree_per_step # angle to absolute steps
+        rotation_times = required_steps*1000000/self.PWM_frequency
         
         for i in range(3):
-            pi.write(MODE[i], RESOLUTION[resolution][i])
+            if relative_angles[i] != 0:
+                step_direction = np.where(relative_angles[i] > 0, 1, 0)
+                self.pi.write(self.DIR[i], step_direction)
+                self.pi.set_PWM_dutycycle(self.STEP[i], 128)
+
+        start_tick = self.pi.get_current_tick()
+        while self.pi.get_current_tick() - start_tick < max(rotation_times):
+            for i in range(3):
+                if self.pi.get_current_tick() - start_tick >= rotation_times[i]:
+                    self.pi.set_PWM_dutycycle(self.STEP[i], 0)
         
-
-    def initial_position():
-        pass
-
-    def set_angle(direction, steps, angle, current_step, resolution):
-
-        absolute_steps = (angle / 1.8) * resolution # angle to absolute steps
-
-        relative_steps = absolute_steps - current_step
-
-        if relative_steps < 0:
-            direction = 
-            pi.write(DI, 1)
-
-
-
-
-
-
-
-    def clean_up():
+        for i in range(3):
+            self.pi.set_PWM_dutycycle(self.STEP[i])
+                
+    def clean_up(self):
+        for i in range(3):
+            self.pi.set_PWM_dutycycle(self.STEP[i])
+        self.pi.stop()
--- setup_pins.py
+++ setup_pins.py
@@ -1,5 +1,7 @@
-# Pins for micro stepping resolution
-MODE = (14, 15, 18)   # Microstep Resolution GPIO Pins
+import numpy as np
 
-direction_pins = [20, 7, 23] # directions pins for motors 1-3
-step_pins = [21, 8, 24] # step pins for motors 1-3
+# Pins for micro stepping resolution
+MODE = np.array([14, 15, 18])   # Microstep Resolution GPIO Pins
+
+direction_pins = np.array([20, 7, 23]) # directions pins for motors 1-3
+step_pins = np.array([21, 8, 24]) # step pins for motors 1-3
