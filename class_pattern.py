5aa1478a3410d264f4d491a5251b519bab363d14
nico <nico.gunnarsson@live.se>
2024-10-30 08:45:07 +0100

    Fixed a potential bug that redefines index-variable to 0 everytime the function was called. Should be fixed now.

--- class_pattern.py
+++ class_pattern.py
@@ -5,55 +5,61 @@ import time
 class Pattern:
 	def __init__(self):
 		self.goal_rad = 50
+		self.index = 0
 
 	def pattern1(self, current):
 		# HEXAGON
 
 		# Manually set positions for ball
 		set_points = np.array[[x1, y1], [x2, y2], [x3, y3], [x4, y4], [x5, y5], [x6, y6]]
-		index = 0
+		self.index = self.index % set_points.shape(0) 
 		
 		# Distance between ball and set_position
-		distance = math.sqrt((current(1)-set_points[index, 1])^2 + (current(2)-set_points[index, 2])^2)
+		distance = math.sqrt((current(0)-set_points[self.index, 0])^2 + (current(1)-set_points[self.index, 1])^2)
 		
 		if(distance > self.goal_rad):
-			return set_points[index, :]
+			return set_points[self.index, :]
 		else:
 			# Loops through set_points matrices rows with index
-			index = (index + 1) % set_points.shape(0) 
-			return set_points[index, :]
+			self.index = (self.index + 1)
+			return set_points[self.index, :]
 
 
 	def pattern2(self, current):
 		# SQUARE
+
+		# Manually set positions for ball
 		set_points = np.array[[x1, y1], [x2, y2], [x3, y3], [x4, y4]]
-		index = 0
-		
+		self.index = self.index % set_points.shape(0)
+
 		# Distance between ball and set_position
-		distance = math.sqrt((current(1)-set_points[index, 1])^2 + (current(2)-set_points[index, 2])^2)
+		distance = math.sqrt((current(0)-set_points[self.index, 0])^2 + (current(1)-set_points[self.index, 1])^2)
 		
 
 		if(distance > self.goal_rad):
-			return set_points[index, :]
+			return set_points[self.index, :]
 		else:
 			# Loops through set_points matrices rows with index
-			index = (index + 1) % set_points.shape(0) 
-			return set_points[index, :]
+			self.index = (self.index + 1)
+			return set_points[self.index, :]
 	
 	def pattern3(self, current):
 		# TRIANGLE
+
+		# Manually set positions for ball
 		set_points = np.array[[x1, y1], [x2, y2], [x3, y3]]
-		index = 0
+		self.index = self.index % set_points.shape(0)
+
 		# Distance between ball and set_position
-		distance = math.sqrt((current(1)-set_points[index, 1])^2 + (current(2)-set_points[index, 2])^2)
+		distance = math.sqrt((current(0)-set_points[self.index, 0])^2 + (current(1)-set_points[self.index, 1])^2)
 		
 
 		if(distance > self.goal_rad):
-			return set_points[index, :]
+			return set_points[self.index, :]
 		else:
 			# Loops through set_points matrices rows with index
-			index = (index + 1) % set_points.shape(0) 
-			return set_points[index, :]
+			self.index = (self.index + 1) 
+			return set_points[self.index, :]
 
 	def pattern4(self, start_time):
 		time = time.time()-start_time # index for curve
