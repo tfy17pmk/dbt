7fb95c0102984b7c12544ecb8c4e18d067f4a30f
Polly <polly@cs.umu.se>
2024-11-08 13:49:11 +0100

    Removed unused files and directories. Renamed initial directory to Motors from Main.

--- setup_pins.py
+++
@@ -1,7 +0,0 @@
-import numpy as np
-
-# Pins for micro stepping resolution
-MODE = np.array([14, 15, 18])   # Microstep Resolution GPIO Pins
-
-direction_pins = np.array([20, 7, 23]) # directions pins for motors 1-3
-step_pins = np.array([21, 8, 24]) # step pins for motors 1-3
