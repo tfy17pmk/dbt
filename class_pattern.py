d1c38d5f7ac9e64f13db9c819aca92a93bfc1323
nico <nico.gunnarsson@live.se>
2024-11-13 11:15:16 +0100

    Remade the class completely and made it so the class-function takes current_pos and a queue as input and process through the queue in a way that it returns only one set of coordinates for the PID to process later.

--- class_pattern.py
+++ class_pattern.py
@@ -3,35 +3,31 @@ import numpy as np
 import time
 
 class Pattern:
-	def __init__(self):
-		self.goal_rad = 50
-		self.index = 0
+    def __init__(self):
+        self.goal_rad = 50 
+        self.index = 0
 
-	def pattern1(self, current, goal):
-		# HEXAGON
+    def process_queue(self, current, goal):
+        self.index = self.index % goal.shape(0) 
 
-		# Manually set positions for ball
-		#set_points = np.array[[x1, y1], [x2, y2], [x3, y3], [x4, y4], [x5, y5], [x6, y6]]
-		self.index = self.index % goal.shape(0) 
-		
-		# Distance between ball and set_position
-		distance = math.sqrt((current(0)-goal[self.index, 0])^2 + (current(1)-goal[self.index, 1])^2)
-		
-		if(distance > self.goal_rad):
-			return goal[self.index, :]
-		else:
-			# Loops through set_points matrices rows with index
-			self.index = (self.index + 1)
-			return goal[self.index, :]
+        # Distance between ball and set_position
+        distance = math.sqrt((current(0)-goal[self.index, 0])^2 + (current(1)-goal[self.index, 1])^2)
 
-	def pattern4(self, start_time):
-		time = time.time()-start_time # index for curve
-		A = 3 # parameters for Lissajous curve
-		B = 2
-		a = 2
-		b = 2
-		delta = math.pi/4
-		reference_x = A*math.sin(a*(time) + delta) #reference for x position
-		reference_y = B*math.sin(b*(time)) # reference for y position
+        if(distance > self.goal_rad):
+            return goal[self.index, :]
+        else:
+            # Loops through set_points matrices rows with index
+            self.index = (self.index + 1)
+            return goal[self.index, :]
 
-		return reference_x, reference_y
+    '''def pattern4(self, start_time):
+        time = time.time()-start_time # index for curve
+        A = 3 # parameters for Lissajous curve
+        B = 2
+        a = 2
+        b = 2
+        delta = math.pi/4
+        reference_x = A*math.sin(a*(time) + delta) #reference for x position
+        reference_y = B*math.sin(b*(time)) # reference for y position
+
+        return reference_x, reference_y'''
