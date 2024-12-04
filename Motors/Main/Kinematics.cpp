7b6297001f980ea3cb87f2f847572a635659abb6
Gustav <gurraj00@hotmail.se>
2024-12-04 15:17:11 +0100

    Add commented motor and kinematics code plus clean up

--- Motors/Main/Kinematics.cpp
+++ Motors/Main/Kinematics.cpp
@@ -1,3 +1,11 @@
+/*
+ * This file is part of the ball ballancing robot.
+ *
+ * Developed for Curiosum during the design bulid test course, fall 2024 by 
+ * project group 11.
+ *
+ * date: 4/12-2024
+ */
 #include "Arduino.h"
 #include <stdio.h>
 #include <array>
@@ -9,6 +17,13 @@
 Kinematics::Kinematics() {
 }
 
+/*
+* Description: Calculates motor angles from the normal vector of the table
+*
+* Input: which leg, height of table, normal vector component in x and y direction. 
+*
+* Output: motor angle in degrees
+*/
 double Kinematics::thetas(int leg, double hz, double nx, double ny) {
 
     //create unit normal vector
@@ -22,32 +37,32 @@ double Kinematics::thetas(int leg, double hz, double nx, double ny) {
       _y = _d + (_e / 2) * (1 - (pow(nx, 2) + 3 * pow(nz, 2) + 3 * nz) / (nz + 1 - pow(nx, 2) + (pow(nx, 4) - 3 * pow(nx, 2) * pow(ny, 2)) / ((nz + 1) * (nz + 1 - pow(nx, 2)))));
       _z = hz + _e * ny;
       _mag = sqrt(pow(_y, 2) + pow(_z, 2));
-      //Serial.println(_y / _mag) + acos((pow(_mag, 2) + pow(_f, 2) - pow(_g, 2)) / (2 * _mag * _f));
-      _clamped = (pow(_mag, 2) + pow(_f, 2) - pow(_g, 2)) / (2 * _mag * _f);
-      //_clamped = constrain(_clamped, -1, 1);
-      _angle = acos(_y / _mag) + acos(_clamped);
+      _angle = acos(_y / _mag) + acos((pow(_mag, 2) + pow(_f, 2) - pow(_g, 2)) / (2 * _mag * _f));
       break;
     case leg2:  //Leg B
       _x = (sqrt(3) / 2) * (_e * (1 - (pow(nx, 2) + sqrt(3) * nx * ny) / (nz + 1)) - _d);
       _y = _x / sqrt(3);
       _z = hz - (_e / 2) * (sqrt(3) * nx + ny);
       _mag = sqrt(pow(_x, 2) + pow(_y, 2) + pow(_z, 2));
-      _clamped = (pow(_mag, 2) + pow(_f, 2) - pow(_g, 2)) / (2 * _mag * _f);
-      //_clamped = constrain(_clamped, -1, 1);
-      _angle = acos((sqrt(3) * _x + _y) / (-2 * _mag)) + acos(_clamped);
+      _angle = acos((sqrt(3) * _x + _y) / (-2 * _mag)) + acos((pow(_mag, 2) + pow(_f, 2) - pow(_g, 2)) / (2 * _mag * _f));
       break;
     case leg3:  //Leg C
       _x = (sqrt(3) / 2) * (_d - _e * (1 - (pow(nx, 2) - sqrt(3) * nx * ny) / (nz + 1)));
       _y = -_x / sqrt(3);
       _z = hz + (_e / 2) * (sqrt(3) * nx - ny);
       _mag = sqrt(pow(_x, 2) + pow(_y, 2) + pow(_z, 2));
-      _clamped = (pow(_mag, 2) + pow(_f, 2) - pow(_g, 2)) / (2 * _mag * _f);
-      //_clamped = constrain(_clamped, -1, 1);
-      _angle = acos((sqrt(3) * _x - _y) / (2 * _mag)) + acos(_clamped);
+      _angle = acos((sqrt(3) * _x - _y) / (2 * _mag)) + acos((pow(_mag, 2) + pow(_f, 2) - pow(_g, 2)) / (2 * _mag * _f));
       break;
   }
   return (_angle * (180 / pi));  //converts angle to degrees and returns the value
 }
+/*
+* Description: Constrain normal vector component in x and y direction and compute motor angles 
+*
+* Input: normal vector component in x and y direction and the height of the table
+*
+* Output: motor angles in degrees
+*/
 std::array<double, 3> Kinematics::setPosition(double normal_vector_x, double normal_vector_y, double _height) {
   // Set boundaries for table angles
   if (normal_vector_x > _max_normal_x) {
--- Motors/Main/Motors.h
+++ Motors/Main/Motors.h
@@ -1,3 +1,11 @@
+/*
+ * This file is part of the ball ballancing robot.
+ *
+ * Developed for Curiosum during the design bulid test course, fall 2024 by 
+ * project group 11.
+ *
+ * date: 4/12-2024
+ */
 #ifndef MOTORS_H
 #define MOTORS_H
 
@@ -17,24 +25,15 @@ public:
     void IRAM_ATTR handleButtonPress2();
     void IRAM_ATTR handleButtonPress3();
 
-    //const int _buttonPin1 = 25;    // Button pin (limit switch or homing switch)
-
-    //const int _buttonPin2 = 5;    // Button pin (limit switch or homing switch)
-
-    //const int _buttonPin3 = 4;    // Button pin (limit switch or homing switch)
+    // set pins for calibration buttons
     std::array<int, 3> buttonPin = {25, 5, 4};
     std::array<bool, 3> buttonPressed;
+
     std::array<bool, 3> skipPhaseOne;
-    double speed1[3] = {0,0,0};
-    double speed2[3] = {0,0,0};
-    double speed3[3] = {0,0,0};
+
+    // initialize speed and position vector
     double speed[3] = {0,0,0};
-
     double position[3] = {0,0,0};
-    double _diff[3] = {0,0,0};
-
-
-
 
 private:
     // Member variables for motor control
@@ -47,20 +46,26 @@ private:
 
     int _RESOLUTION[3] = {1,0,1}; // Resolution for 1/32 microstepping 
     float _inv_degree_per_step = 17.777778; // Set inverse degree per step 1/(1.8/32);
-    std::array<double, 3> _steps = {0,0,0};
-    std::array<double, 3> prev_steps = {0,0,0};
-    void set_speed(double steps[3]);
-    float _max_speed = 2000;//2000
-    float _cs = 50;//50
-    double _acc_multiplier = 5;//5
-    double _speed_diff = 100;//100
-    double speedPrev[3];
-    double prev_rec_time = 0;
 
+    // initialize arrays
+    std::array<double, 3> _steps = {0,0,0};
+    std::array<double, 3> _prev_steps = {0,0,0};
+    double _speedPrev[3];
+    double _diff[3] = {0,0,0};
+
+    // initialize functions
+    void set_speed(double steps[3]);
     void phase1();
     void phase2(int motor, bool first_time);
     void phase3();
 
+    // set constants for motor characteristics
+    float _max_speed = 2000;//2000
+    float _cs = 50;//50
+    double _acc_multiplier = 5;//5
+    double _speed_diff = 100;//100
+
+    double _prev_rec_time = 0;
 };
 
 #endif // MOTORS_H
