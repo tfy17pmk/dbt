703297fb3144266f671add891a0e68b439fda996
Malin <malin00gidlund@gmail.com>
2024-12-11 15:01:07 +0100

    Commented pattern page and hexagon, removed unused function

--- GUI/Hexagon.py
+++ GUI/Hexagon.py
@@ -6,13 +6,16 @@ import numpy as np
 from scipy.spatial import distance
 
 class HexagonShape:
+    """Handles pattern page drawing board interactions"""
+
     def __init__(self, canvas, fill="#ffffff", outline="#ffffff"):
+        "Initialize drawing board"
         self.canvas = canvas
         self.points = []  # Hexagon corner points
         self.fill = fill
         self.outline = outline
         self.drawing_points = []  # Store drawing points
-        self.mapped_points = [] # Store remapped points
+        self.mapped_points = [] # Store remapped drawing points
         self.line_ids = []  # Store IDs for each complete drawn line
         self.current_line_ids = []  # Track segment IDs for current line
         self.is_freehand = False  # Track if drawing is freehand
@@ -26,7 +29,7 @@ class HexagonShape:
         self.restart_delay = 0.01
         self.epsilon = 15 # deviation for draw patterns optimization
 
-        # Bind to draw the hexagon in the case of window resize
+        # Bind to draw the hexagon in the case of window resizing
         self.canvas.bind("<Configure>", self.on_resize)
 
         # Bind to handle mouse interactions
@@ -39,17 +42,20 @@ class HexagonShape:
         self.start_thread()
         
     def send_data(self):
+        """Send new goal positions to main"""
         while not self.stop_event.is_set():
             try:
                 if not self.data_queue.empty():
+                    # Get distance from previous position to new goal
                     coordinates = self.data_queue.get_nowait()
-                    
                     dist = math.sqrt((self.prev_goal_pos[0] - coordinates[0]) ** 2 + (self.prev_goal_pos[1] - coordinates[1]) ** 2)
+                    # Send new goal position and delay for time it will take to reach new goal position
                     delay = abs((dist/80) - self.restart_delay)
                     self.send_goal_pos(coordinates[0], coordinates[1])
                     self.prev_goal_pos = coordinates
                     time.sleep(delay)
                 elif len(self.mapped_points):
+                    # If there are mapped points put them in data queue
                     [self.data_queue.put((x,y)) for x, y in self.mapped_points]
                 time.sleep(self.restart_delay)  # Add a small delay to observe the behavior
         
@@ -57,28 +63,27 @@ class HexagonShape:
                 pass
         
     def start_thread(self):
+        "Start thread sending goal positions to main"
         self.thread_started = True
         self.stop_event.clear()
         self.thread = threading.Thread(target=self.send_data)
         self.thread.start()
     
     def set_goal_function(self, send_goal_pos_function):
+        """Connect to pattern page function sending goal positions to main"""
         self.send_goal_pos = send_goal_pos_function
         
     def clear_thread(self):
-
+        """Empty data queue and set origin as goal position"""
+        # Clear mapped points and data queue
         self.mapped_points = []
         while not self.data_queue.empty():
             self.data_queue.get_nowait()
         
         self.send_goal_pos(0, 0)
-        ''' # If we want to kill the thread 
-        self.stop_event.set()
-        self.thread.join()
-        self.thread_started = False
-        '''
           
     def clear_all(self):
+        """Clear drawing board and data queue"""
         # Function to overwrite previous goal points
         while not self.data_queue.empty():
             self.data_queue.get_nowait()
@@ -93,7 +98,7 @@ class HexagonShape:
         self.canvas.delete("shape")
 
     def reset_mapped_points(self):
-        # Function for reseting position to [0, 0] when reseting pattern or going back to prev GUI slide
+        """Resets position to origin when pressing undo or returning to prev page"""
 
         # Clear all lines
         while self.line_ids:
@@ -111,6 +116,8 @@ class HexagonShape:
         self.send_goal_pos(0, 0)
 
     def map_coordinates(self, x, y):
+        """Remap position data to balancing plate dimensions with origin in the center"""
+        # Get screen dimensions
         canvas_width = self.canvas.winfo_width()
         canvas_height = self.canvas.winfo_height()
 
@@ -121,6 +128,7 @@ class HexagonShape:
         if not self.thread_started:
             self.start_thread()
         
+        # Put coordinate data in data queuee
         if not self.data_queue.full():
             try:
                 self.data_queue.put((int(mapped_x-self.target_width/2), int(mapped_y-self.target_height/2)), timeout=0.01)
@@ -129,28 +137,35 @@ class HexagonShape:
         else:
             print(f"Queue with goal pos is full!")
             
-        # Return remapped coordinates with redefined origin in the middle    
+        # Return remapped coordinates   
         return int(mapped_x-self.target_width/2), int(mapped_y-self.target_height/2) 
         
     def log_shape_coordinates(self, points):
-        # Example of mapping coordinates (optional)
+        """Puts mapped coordinates into array"""
         self.mapped_points = [self.map_coordinates(x, y) for x, y in points]
         print("Mapped shape coordinates:", self.mapped_points)
 
     def draw_square(self):
+        """Draw square on drawing board and get mapped goal position coordinates"""
         self.is_freehand = False
         self.clear_all()
+
+        # Calculate the width, height of the canvas
         width = self.canvas.winfo_width()
         height = self.canvas.winfo_height()
+
+        # Calculate corner points and draw square on drawing board
         size = min(width, height) // 2
         x0, y0 = (width - size) / 2, (height - size) / 2
         x1, y1 = (width + size) / 2, (height + size) / 2
         shape_id = self.canvas.create_rectangle(x0, y0, x1, y1, outline="black", tags="shape")
         self.line_ids.append([shape_id])
+
         # Log the coordinates of the square
         self.log_shape_coordinates([(x0, y0), (x1, y0), (x1, y1), (x0, y1), (x0, y0)])
 
     def draw_hexagon(self):
+        """Draw hexagon on drawing board and get mapped goal position coordinates"""
         self.is_freehand = False
         self.clear_all()
 
@@ -170,19 +185,25 @@ class HexagonShape:
             y = center_y + radius * math.sin(angle_rad)
             points.append((x, y))
         points.append(points[0])
+
         # Draw the hexagon on the canvas
         shape_id = self.canvas.create_polygon(points, outline="black", fill="", tags="shape")
         self.line_ids.append([shape_id])
 
-        # Log the coordinates of the hexagon vertices
+        # Log the coordinates
         self.log_shape_coordinates(points)
 
     def draw_triangle(self):
+        """Draw triangle on drawing board and get mapped goal position coordinates"""        
         self.is_freehand = False
         self.clear_all()
+
+        # Calculate the width, height of the canvas
         width = self.canvas.winfo_width()
         height = self.canvas.winfo_height()
         size = min(width, height) // 2
+        
+        # Calculate points of triangle and draw on drawing board
         points = [
             (width / 2, (height - size) / 2),
             ((width - size) / 2, (height + size) / 2),
@@ -191,10 +212,12 @@ class HexagonShape:
         ]
         shape_id = self.canvas.create_polygon(points, outline="black", fill="", tags="shape")
         self.line_ids.append([shape_id])
-        # Log the coordinates of the triangle vertices
+
+        # Log the coordinates
         self.log_shape_coordinates(points)
 
     def draw_star(self):
+        """Draw star on drawing board and get mapped goal position coordinates"""
         self.is_freehand = False
         self.clear_all()
 
@@ -203,11 +226,11 @@ class HexagonShape:
         height = self.canvas.winfo_height()
         center_x = width / 2
         center_y = height / 2
-        outer_radius = min(width, height) / 2.5  # Radius for the hexagon to fit within canvas
+        outer_radius = min(width, height) / 2.5
         inner_radius = outer_radius/3
         starpoints = 5
 
-        # Calculate the six vertices of the hexagon
+        # Calculate the points of the star and draw on drawing board
         points = []
         for i in range(starpoints*2):
             angle  = (i-0.5) * math.pi / starpoints
@@ -216,14 +239,14 @@ class HexagonShape:
             y = center_y + radius * math.sin(angle)
             points.append((x, y))
         points.append(points[0])
-        # Draw the hexagon on the canvas
         shape_id = self.canvas.create_polygon(points, outline="black", fill="", tags="shape")
         self.line_ids.append([shape_id])
 
-        # Log the coordinates of the hexagon vertices
+        # Log the coordinates
         self.log_shape_coordinates(points)
 
     def draw_heart(self):
+        """Draw heart on drawing board and get mapped goal position coordinates"""
         self.is_freehand = False
         self.clear_all()
 
@@ -232,11 +255,10 @@ class HexagonShape:
         height = self.canvas.winfo_height()
         center_x = width / 2
         center_y = height / 2
-        #radius = min(width, height) / 2.5  # Radius for the hexagon to fit within canvas
         scale = 12.5
         pointnumber = 20
 
-        # Calculate the six vertices of the hexagon
+        # Calculate the points of the heart and draw on drawing board
         points = []
         for i in range(pointnumber):
             t = i * 2 * math.pi / pointnumber  # Generate n_points evenly spaced values for t
@@ -244,14 +266,14 @@ class HexagonShape:
             y = -scale * (13 * np.cos(t) - 5 * np.cos(2 * t) - 2 * np.cos(3 * t) - np.cos(4 * t)) + center_y
             points.append((x, y))
         points.append(points[0])
-        # Draw the hexagon on the canvas
         shape_id = self.canvas.create_polygon(points, outline="black", fill="", tags="shape")
         self.line_ids.append([shape_id])
 
-        # Log the coordinates of the hexagon vertices
+        # Log the coordinates
         self.log_shape_coordinates(points)
 
     def draw_circle(self):
+        """Draw heart on drawing board and get mapped goal position coordinates"""
         self.is_freehand = False
         self.clear_all()
 
@@ -260,9 +282,9 @@ class HexagonShape:
         height = self.canvas.winfo_height()
         center_x = width / 2
         center_y = height / 2
-        radius = min(width, height) / 2.5  # Radius for the hexagon to fit within canvas
+        radius = min(width, height) / 2.5
 
-        # Calculate the six vertices of the hexagon
+        # Calculate the points of the circle and draw on drawing board
         points = []
         for i in range(45):
             angle_deg = 8 * i
@@ -271,14 +293,14 @@ class HexagonShape:
             y = center_y + radius * math.sin(angle_rad)
             points.append((x, y))
         points.append(points[0])
-        # Draw the hexagon on the canvas
         shape_id = self.canvas.create_polygon(points, outline="black", fill="", tags="shape")
         self.line_ids.append([shape_id])
 
-        # Log the coordinates of the hexagon vertices
+        # Log the coordinates
         self.log_shape_coordinates(points)
     
     def clear_shapes_if_present(self):
+        """Clear any shapes on the drawing board"""
         # Check if any shapes are in line_ids and remove them
         if self.line_ids:
             # Filter out all items tagged as "shape" from line_ids
@@ -287,6 +309,7 @@ class HexagonShape:
             self.canvas.delete("shape")
 
     def create_hexagon(self):
+        """Create drawing board hexagon"""
         # Clear canvas
         self.canvas.delete("all")
         
@@ -310,11 +333,12 @@ class HexagonShape:
         self.canvas.create_polygon(self.points, outline=self.outline, fill=self.fill, width=2)
 
     def on_resize(self, event):
-        # Rescale the hexagon in case of window size changing
+        """Rescale the hexagon in case of window size changing"""
         self.create_hexagon()
         self.redraw_points()  # Redraw points on resize
 
     def start_drawing(self, event):
+        """If touching screen within drawing board boundaries, enable drawing"""
         self.clear_all()
 		# Start drawing if inside hexagon
         if self.is_point_inside_hexagon(event.x, event.y):
@@ -324,7 +348,7 @@ class HexagonShape:
             self.drawing_points.append((event.x, event.y))  # Register initial point
 
     def draw(self, event):
-        # Capture points as the mouse moves, only add if distance is sufficient
+        """Capture points as the mouse moves, only add if distance from last point is sufficient"""
         if self.is_point_inside_hexagon(event.x, event.y):
             if not self.drawing_points:
                 self.drawing_points.append((event.x, event.y))
@@ -338,6 +362,7 @@ class HexagonShape:
                     self.drawing_points.append((event.x, event.y))
 
     def stop_drawing(self, event):
+        """Append drawned points to mapped points if user is no longer drawing"""
         self.clear_all()
         self.mapped_points = []
         # Connect the last point to the first to close the shape, if close enough
@@ -359,14 +384,14 @@ class HexagonShape:
         print("Mapped drawing points", self.mapped_points)
 
     def redraw_points(self):
-        # Redraw the stored lines between points
+        """Redraw the stored lines between points"""
         for line in self.line_ids:
             for line_id in line:
                 # Canvas automatically keeps deleted segments removed
                 pass
 
     def is_point_inside_hexagon(self, x, y):
-        # Point-within-polygon test (Ray-casting algorithm)
+        """Point-within-polygon test (Ray-casting algorithm)"""
         n = len(self.points)
         inside = False
         p1x, p1y = self.points[0]
@@ -381,6 +406,7 @@ class HexagonShape:
                             inside = not inside
             p1x, p1y = p2x, p2y
         return inside
+    
     def perpendicular_distance(self, point, line_start, line_end):
         """
         Calculate the perpendicular distance of a point from a line.
@@ -439,15 +465,7 @@ class HexagonShape:
             return [start, end]
         
     def join_threads(self):
+        "Kill thread"
         self.stop_event.set()
         if hasattr(self, 'thread') and self.thread.is_alive():
             self.thread.join()
-
-    
-    '''def remove_last_line(self):
-        # Remove the last drawn line or shape
-        if self.line_ids:
-            last_item = self.line_ids.pop()
-            print("Funkade najs (interupt grej)!!")
-            for item_id in last_item:
-                self.canvas.delete(item_id)'''
